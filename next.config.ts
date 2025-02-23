1020e273f0d281f465c778ba5e10f8cf21af7cef
Aadiaks22 <adityaaks220@gmail.com>
2025-02-23 12:31:23 +0530

    Createing build package

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Enables static exports
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
 };
 
 export default nextConfig;
